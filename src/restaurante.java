4cfc602c10550845e9570ada624015245ec12eed
Paulo Andrade <ppvra@outlook.com>
2024-09-15 23:24:48 -0300

    Pronto para entrega

--- src/restaurante.java
+++ src/restaurante.java
@@ -2,55 +2,61 @@ import java.util.Scanner;
 public class restaurante {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Bem vindo ao Mequi feliz\n"  + "digite o seu nome:");
-        
-                        Scanner nome = new Scanner(System.in);
-                        String nomeIn = nome.nextLine();
-                                         
+        System.out.println("Bem vindo(a) ao Mequi feliz\n"  + "digite o seu nome:");
+        Scanner scanner = new Scanner(System.in);
+        String nomeIn = scanner.nextLine();
 
-        System.out.println("Bem-vindo ao Mequi feliz! " + nomeIn + "\n" +
-                            "É um prazer receber você em nosso espaço, onde cada refeição é preparada com Alegria, carinho e ingredientes de qualidade\n" +
-                            "A diversão do Mequi esse mes é a turmar do sonic, onde voce pode escolher um entre esses brinquedos:\n" +
-                            "(1)Sonic\n" +"(2)Shadow\n" +"(3)Tails\n" +"(4)Amy Rose\n" +"(5)knuckles\n" );
+                                         
+        System.out.println(
+        "\nBem-vindo(a) ao Mequi feliz! " + nomeIn  +
+        "\nÉ um prazer receber você em nosso espaço, onde cada refeição é preparada com Alegria, carinho e ingredientes de qualidade\n" +
+        "A diversão do Mequi esse mes é a turmar do sonic, onde voce pode escolher um entre esses brinquedos:\n" +
+        "(1) R$10: Sonic\n" +
+        "(2) R$10: Shadow\n" +
+        "(3) R$10: Tails\n" +
+        "(4) R$10: Amy Rose\n" +
+        "(5) R$10: knuckles\n");
+
         
-        System.out.println("Favor\n digite o codigo de qual diversão deseja! ");
-        Scanner codigoDiversão = new Scanner(System.in);
-        int codigoDiver01 = codigoDiversão.nextInt();
+        System.out.println("Favor\nDigite o codigo de qual diversão deseja! ");
+        int codigoDiver01 = scanner.nextInt();
         String diversãoEscolhida="";
+        double preçoDiversão=10;
                         
         if (codigoDiver01 == 1) 
         {diversãoEscolhida="sonic";
-            System.out.println("Certo!\n"+ "voce escolheu o Sonic\n"+ "agora vamos escolher oq deseja comer!\n");}
-        else{
-            if (codigoDiver01 == 2)
-            {diversãoEscolhida="shadow";
-                System.out.println("Certo!\n"+"voce escolheu o Shadow\n"+"agora vamos escolher oq deseja comer!\n");}
-                else{
-                    if (codigoDiver01==3) 
-                    {diversãoEscolhida="tails";
-                        System.out.println("Certo!\n"+ "voce escolheu o Tails\n"+ "agora vamos escolher oq deseja comer!\n");}
-                    else{
-                        if (codigoDiver01==4) 
-                        {diversãoEscolhida="Amy Rose";
-                            System.out.println("Certo!\n"+ "voce escolheu a Amy Rose\n"+ "agora vamos escolher oq deseja comer!\n");}
-                        else{
-                            if (codigoDiver01==5) 
-                            {diversãoEscolhida="Knuckles";
-                                System.out.println("Certo!\n"+"voce escolheu o Knuckles\n"+  "agora vamos escolher oq deseja comer!\n");}
-                            }
-                        }
-                    }
-            }
-        System.out.println("as opçoes de prato principal são as seguintes\n"+
-                            "(6).MequiFiesta\n"+
-                            "(7).MequiChedar\n"+
-                            "(8).MequiChicken\n"+
-                            "(9).MequiRanch\n"+
-                            "(10).MequiFish\n"+
-                            "favor digite o codigo do prato principal\n");
+        System.out.println("Certo!\n"+ "voce escolheu o Sonic\n"+ "agora vamos escolher oq deseja comer!\n");}
         
-        Scanner codigoPratoPrincipal = new Scanner(System.in);
-        int codigoPrato01 = codigoPratoPrincipal.nextInt();
+        else if (codigoDiver01 == 2)
+        {diversãoEscolhida="shadow";
+        System.out.println("Certo!\n"+"voce escolheu o Shadow\n"+"agora vamos escolher oq deseja comer!\n");}
+        
+        else if (codigoDiver01==3) 
+        {diversãoEscolhida="tails";
+        System.out.println("Certo!\n"+ "voce escolheu o Tails\n"+ "agora vamos escolher oq deseja comer!\n");}
+        
+        else if (codigoDiver01==4) 
+        {diversãoEscolhida="Amy Rose";
+        System.out.println("Certo!\n"+ "voce escolheu a Amy Rose\n"+ "agora vamos escolher oq deseja comer!\n");}
+        
+        else if (codigoDiver01==5) 
+        {diversãoEscolhida="Knuckles";
+        System.out.println("Certo!\n"+"voce escolheu o Knuckles\n"+  "agora vamos escolher oq deseja comer!\n");}
+        
+        else{System.out.println("opção invalida, favor digitar codigo igual exposto no menu");
+        return;}
+                            
+        
+        System.out.println(
+        "as opçoes de prato principal são as seguintes\n"+
+        "(6) R$10.00: MequiFiesta\n"+
+        "(7) R$10.50: MequiChedar\n"+
+        "(8) R$10.75: MequiChicken\n"+
+        "(9) R$11.00: MequiRanch\n"+
+        "(10) R$11.50: MequiFish\n"+
+        "favor digite o codigo do prato principal\n");
+        
+        int codigoPrato01 = scanner.nextInt();
         String pratoEscolhido="";
         double preçoPrato= 0;
         
@@ -58,80 +64,82 @@ public class restaurante {
         {pratoEscolhido="MequiFiesta";
         preçoPrato=10.00;
         System.out.println("Certo!\n"+ "voce escolheu o MequiFiesta\n"+ "agora vamos escolher o acompanhamento!");}
-        else{
-            if (codigoPrato01 == 7)
-            {pratoEscolhido="MequiChedar";
-            preçoPrato=10.50;
-            System.out.println("Certo!\n"+"voce escolheu o MequiChedar\n"+"agora vamos escolher o acompanhamento!!");}
-                else{
-                    if (codigoPrato01==8) 
-                    {pratoEscolhido="MequiChiken";
-                    preçoPrato=10.75;
-                    System.out.println("Certo!\n"+ "voce escolheu o MequiChicken\n"+ "agora vamos escolher o acompanhamento!!");}
-                    else{
-                        if (codigoPrato01==9) 
-                        {pratoEscolhido="MequiRanch";
-                        preçoPrato=11.00;
-                        System.out.println("Certo!\n"+ "voce escolheu o MequiRanch\n"+ "agora vamos escolher o acompanhamento!!");}
-                        else{
-                            if (codigoPrato01==10) 
-                            {pratoEscolhido="MequiFish";
-                            preçoPrato=11.50;
-                            System.out.println("Certo!\n"+"voce escolheu o Mequifish\n"+  "agora vamos escolher o acompanhamento!!");}
-                            }
-                        }
-                    }
-     
-            }
-        System.out.println("as opçoes de Acompanhamento são as seguintes\n"+
-                            "(11).Mequi Fritas pequena\n"+
-                            "(12).Mequi firtas media\n"+
-                            "(13).Mequi Fritas grande\n"+
-                            "(14).Mequi nuggets 6 unidades\n"+
-                            "(15).Mequi nuggets 10 unidades\n"+
-                            "favor digite o codigo do Acomponhamento\n");
-                            
-        Scanner codigoAcompanhamento = new Scanner(System.in);
-        int codigoAcompanhamento01 = codigoAcompanhamento.nextInt();
+        
+        else if (codigoPrato01 == 7)
+        {pratoEscolhido="MequiChedar";
+        preçoPrato=10.50;
+        System.out.println("Certo!\n"+"voce escolheu o MequiChedar\n"+"agora vamos escolher o acompanhamento!!");}
+        
+        else if (codigoPrato01==8) 
+        {pratoEscolhido="MequiChiken";
+        preçoPrato=10.75;
+        System.out.println("Certo!\n"+ "voce escolheu o MequiChicken\n"+ "agora vamos escolher o acompanhamento!!");}
+        
+        else if (codigoPrato01==9) 
+        {pratoEscolhido="MequiRanch";
+        preçoPrato=11.00;
+        System.out.println("Certo!\n"+ "voce escolheu o MequiRanch\n"+ "agora vamos escolher o acompanhamento!!");}
+        
+        else if (codigoPrato01==10) 
+        {pratoEscolhido="MequiFish";
+        preçoPrato=11.50;
+        System.out.println("Certo!\n"+"voce escolheu o Mequifish\n"+  "agora vamos escolher o acompanhamento!!");}
+        
+        else{System.out.println("opção invalida, favor digitar codigo igual exposto no menu");
+        return;}
+            
+        
+        System.out.println(
+        "as opçoes de Acompanhamento são as seguintes\n"+
+        "(11) R$2.50: Mequi Fritas pequena\n"+
+        "(12) R$3.00: Mequi firtas media\n"+
+        "(13) R$4.00: Mequi Fritas grande\n"+
+        "(14) R$7.00:Mequi nuggets 6 unidades\n"+
+        "(15) R$10.00: Mequi nuggets 10 unidades\n"+
+        "favor digite o codigo do Acomponhamento\n");
+                                  
+        int codigoAcompanhamento01 = scanner.nextInt();
         String AcompanhamentoEscolhido="";
         double preçoAcompanhamento=0;
-    
-                            
+                      
         if (codigoAcompanhamento01 == 11) 
         {AcompanhamentoEscolhido="Fritas pequena";
         preçoAcompanhamento=2.50;
         System.out.println("Certo!\n"+ "voce escolheu as Mequi Fritas pequena");}
-        else{
-            if (codigoAcompanhamento01 == 12)
-            {AcompanhamentoEscolhido="Fritas Media";
-            preçoAcompanhamento=3.00;
-            System.out.println("Certo!\n"+"voce escolheu as Mequi Fritas Media");}
-                else{
-                    if (codigoAcompanhamento01==13) 
-                    {AcompanhamentoEscolhido="fritas grande";
-                    preçoAcompanhamento=4.00;
-                    System.out.println("Certo!\n"+ "voce escolheu as Mequi Fritas grande");}
-                    else{
-                        if (codigoAcompanhamento01==14) 
-                        {AcompanhamentoEscolhido="nuggets 6 unidades";
-                        preçoAcompanhamento=5.00;
-                        System.out.println("Certo!\n"+ "voce escolheu: Mequi nuggets 6 unidades");}
-                        else{
-                            if (codigoAcompanhamento01==15) 
-                            {AcompanhamentoEscolhido="nuggets 10 unidades";
-                            preçoAcompanhamento=6.00;
-                            System.out.println("Certo!\n"+"voce escolheu: Mequi nuggets 10 unidades");}
-                            }
-                        }
-                    }
-                         
-            }
         
-        System.out.println(" agora vamos escolher a bebida\n no Mequi feliz temos a bebida padrão refil de 700ml, onde voce escolher o sabor e pode encher quantas vezes quiser\n"+
-                           "As opçoes de bebidas são:\n(16)coca zero\n(17)peps\n(18)fanta\n Favor digite o codigo da bebida:");
+        else if (codigoAcompanhamento01 == 12)
+        {AcompanhamentoEscolhido="Fritas Media";
+        preçoAcompanhamento=3.00;
+        System.out.println("Certo!\n"+"voce escolheu as Mequi Fritas Media");}
+                
+        else if (codigoAcompanhamento01==13) 
+        {AcompanhamentoEscolhido="fritas grande";
+        preçoAcompanhamento=4.00;
+        System.out.println("Certo!\n"+ "voce escolheu as Mequi Fritas grande");}
+                    
+        else if (codigoAcompanhamento01==14) 
+        {AcompanhamentoEscolhido="nuggets 6 unidades";
+        preçoAcompanhamento=7.00;
+        System.out.println("Certo!\n"+ "voce escolheu: Mequi nuggets 6 unidades");}
+                        
+        else if (codigoAcompanhamento01==15) 
+        {AcompanhamentoEscolhido="nuggets 10 unidades";
+        preçoAcompanhamento=10.00;
+        System.out.println("Certo!\n"+"voce escolheu: Mequi nuggets 10 unidades");}
+
+        else{System.out.println("opção invalida, favor digitar codigo igual exposto no menu");
+        return;}
+
+
+        System.out.println(
+        "Agora vamos escolher a bebida\n no Mequi feliz temos a bebida padrão refil de 700ml, onde voce escolher o sabor e pode encher quantas vezes quiser\n"+
+        "As opçoes de bebidas são:\n"+
+        "(16) R$12.00: coca zero\n"+
+        "(17) R$10.00: pepsi\n"+
+        "(18) R$9.00: fanta\n"+
+        "Favor digite o codigo da bebida:");
                            
-        Scanner codigoBebida = new Scanner(System.in);
-        int codigoBebida01 = codigoBebida.nextInt();
+        int codigoBebida01 = scanner.nextInt();
         String bebidaEscolhida="";
         double preçoBebida=0;
         
@@ -139,31 +147,32 @@ public class restaurante {
         {bebidaEscolhida="coca zero";
         preçoBebida=12.00;
         System.out.println("Certo!\n"+ "voce escolheu coca zero pra beber");}
-        else{
-            if (codigoBebida01 == 17)
-            {bebidaEscolhida="Peps";
-            preçoBebida=10.00;
-            System.out.println("Certo!\n"+"voce escolheu Peps pra beber");}
-                else{
-                    if (codigoBebida01==18) 
-                    {bebidaEscolhida="fanta";
-                    preçoBebida=8.00;
-                    System.out.println("Certo!\n"+ "voce escolheu Fanta pra beber");}
-                    }
-            }
-        double total = preçoAcompanhamento+preçoBebida+preçoPrato;
+        
+        else if (codigoBebida01 == 17)
+        {bebidaEscolhida="Pepsi";
+        preçoBebida=10.00;
+        System.out.println("Certo!\n"+"voce escolheu Peps pra beber");}
+                
+        else if (codigoBebida01==18) 
+        {bebidaEscolhida="fanta";
+        preçoBebida=9.00;
+        System.out.println("Certo!\n"+ "voce escolheu Fanta pra beber");}
+
+        else{System.out.println("opção invalida, favor digitar codigo igual exposto no menu");
+        return;}
+
+
+        double total = preçoAcompanhamento+preçoBebida+preçoPrato+preçoDiversão;
+
 
         System.out.println("\nResumo do pedido:");
-        System.out.println("Diversão: " + diversãoEscolhida);
+        System.out.println("Diversão: " + diversãoEscolhida+"(R$ "+preçoDiversão+")");
         System.out.println("Prato Principal: " + pratoEscolhido + " (R$ " + preçoPrato + ")");
         System.out.println("Acompanhamento: " + AcompanhamentoEscolhido + " (R$ " + preçoAcompanhamento + ")");
         System.out.println("Bebida: " + bebidaEscolhida + " (R$ " + preçoBebida + ")");
         System.out.println("Total a pagar: R$ " + total);
 
-
-            
-    //favor não mecher nos cochetes abaixo vv! >.<                        
+        scanner.close();            
+    //favor não mecher nos cochetes abaixo  >.<                        
     }
 }
-
-
